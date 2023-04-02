9c964cf9c4f998a5b753b583edea8ddffa6b69d6
ma.mehralian <ma.mehralian@gmail.com>
2023-04-02 23:06:28 +0430

    error handling and chunked request added

--- examples/2_simple_client/main.cpp
+++ examples/2_simple_client/main.cpp
@@ -5,9 +5,20 @@
 using namespace std;
 
 int main(int argn, char* argc[]) {
-	Client c("http://127.0.0.1:4313");
-	auto req = c.SendRequest(Request::RequestMethod::GET, "/api/color_classes");
-	cout << "calling:" << req.FullUrl() << endl;
-	cout << req.GetContent();
+	Client c("http://127.0.0.1:4110");
+	auto h = [](Request& request) {
+		printf("new chunk:\n %s \n", request.GetContent().c_str());
+	};
+
+	try {
+		auto req = c.SendRequest(Request::RequestMethod::POST, "/engine?service=start");
+		cout << "response: " << req.GetContent() << endl;
+		auto req2 = c.SendChunkedRequest(h, Request::RequestMethod::GET, "/engine/live");
+		cout << "calling:" << req.FullUrl() << endl;
+		cout << "response: " << req2.GetContent();
+	}
+	catch (const exception& e) {
+		cout << "request failed: " << e.what() << endl;
+	}
 	return 0;
 }
